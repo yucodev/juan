42c0c6ad3bf65aa41b97a3cfe5b8426dda431586
viktaur <30535579+viktaur@users.noreply.github.com>
2020-12-03 17:15:15 +0100

    stable?

--- juan.py
+++ juan.py
@@ -18,7 +18,23 @@ import config
 client = discord.Client()
 
 @client.event
+#when the bot started running, we may print its name, id etc
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print('Logged in')
+    print("Username: ", end='')
+    print(client.user.name)
+    print("Userid: ", end='')
+    print(client.user.id)
+@client.event
+#when the bot gets the message this method gets triggered
+async def on_message(message):
+    if message.author.id == client.user.id:
+        return
+    #message starts with hi or hello then print these
+    if message.content.startswith('Hi') or message.content.startswith('Hello'):
+        await message.channel.send('Hello {0.author.mention} Welcome Man'.format(message))
+    #when the message with help then do this
+    elif message.content.startswith('help'):
+        await message.channel.send("Let me check with that level and come back to you")
 
 client.run(config.token)
