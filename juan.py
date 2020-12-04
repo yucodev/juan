fe755def811e3a69873abbc7596cf79b3d7c7699
viktaur <30535579+viktaur@users.noreply.github.com>
2020-12-04 18:17:18 +0100

    lists

--- juan.py
+++ juan.py
@@ -34,15 +34,15 @@ async def on_message(message):
         return
     #message starts with hi or hello then print these
     # LOOKING FORWARD TO SOLVING CASE SENSITIVE PROBLEM!
-    if message.content.startswith('hi') or message.content.startswith('hello'):
+    if message.content.startswith(['hi', 'hello', 'howdy']):
         msg = 'Hello {0.author.mention} welcome man, cómo andás my friend?'.format(message)
         await message.channel.send(msg)
     #when the message with help then do this
-    elif message.content.startswith('help', 'i need help'):
+    elif message.content.startswith(['help', 'i need help']):
         msg = 'Let me check with that level and come back to you, amigo!'.format(message)
         await message.channel.send(msg)
 
-    elif message.content.startswith('random advice', 'give me a random advice'):
+    elif message.content.startswith(['random advice', 'give me a random advice']):
         list = ['Eat tacos and be happy!', 'Horses are nice, i love horses and you should too, compadre', 'study more, do some exercise and that stuff']
         msg = list[random.randint(0, len(list) - 1)].format(message)
         await message.channel.send(msg)
