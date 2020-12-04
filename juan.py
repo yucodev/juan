a28cd07c86b81ccdc957dcb5403ca0fdf40f2321
viktaur <30535579+viktaur@users.noreply.github.com>
2020-12-04 14:57:34 +0100

    random

--- juan.py
+++ juan.py
@@ -10,6 +10,8 @@
 import os
 import discord
 import config
+import random
+import math
 # from dotenv import load_dotenv
 
 # load_dotenv()
@@ -36,8 +38,14 @@ async def on_message(message):
         msg = 'Hello {0.author.mention} welcome man, cómo andás my friend?'.format(message)
         await message.channel.send(msg)
     #when the message with help then do this
-    elif message.content.startswith('help'):
+    elif message.content.startswith('help', 'i need help'):
         msg = 'Let me check with that level and come back to you, amigo!'.format(message)
         await message.channel.send(msg)
+
+    elif message.content.startswith('random advice', 'give me a random advice'):
+        list = ['Eat tacos and be happy!', 'Horses are nice, i love horses and you should too, compadre', 'study more, do some exercise and that stuff']
+        msg = list[random.randint(0, len(list) - 1)].format(message)
+        await message.channel.send(msg)
+
 
 client.run(config.token)
