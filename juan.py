a05e926331e567c43f3bf797cc3fefe9d6840ed4
viktaur <30535579+viktaur@users.noreply.github.com>
2020-12-05 20:23:02 +0100

    comment

--- juan.py
+++ juan.py
@@ -107,11 +107,11 @@ async def on_message(message):
         msg = random.randint(0, 100)
         await message.channel.send(msg)
 
-    # random ge
-    elif str(message.content).lower() in ['random percentage', 'percentage'] or ('likely', 'odds' in str(message.content).lower()):
-        msg = (str(round(random.uniform(0, 100), 2)) + '%').format(message)
-        await message.channel.send(msg)
-        return
+    # random percentage
+    # elif str(message.content).lower() in ['random percentage', 'percentage'] or ('likely', 'odds' in str(message.content).lower()):
+    #     msg = (str(round(random.uniform(0, 100), 2)) + '%').format(message)
+    #     await message.channel.send(msg)
+    #     return
 
     # random integer 1 to 1000
     elif message.content.lower() in ['random number 1000', '1 to 1000']:
