aa19edc358f79312b62d8bbba7fe5f6a7023127a
viktaur <30535579+viktaur@users.noreply.github.com>
2020-12-08 00:22:31 +0100

    not working

--- tests/test_wim.py
+++ tests/test_wim.py
@@ -1,10 +1,11 @@
 def wim(*words):
-    for word in words:
-        # preout = str(word) + " in message.content.lower()"
-        # return preout
-        yield str(word) + " in message.content.lower()"
+        for word in words:
+            preout = str(word) + " in message.content.lower()-"
+            nexed = preout.replace("-", " or ")
+            x = " ".join(line.strip() for line in nexed
+            print(x)
 
-print(wim('hello', 'how are you', 'hola'))
+wim('hello', 'how are you', 'hola')
 
 # x = "".join(wim('hello', 'hi', 'hola'))
 # print(x)
