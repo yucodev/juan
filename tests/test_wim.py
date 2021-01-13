c3b090525088c41862edbe656d0b38c8be311e6a
viktaur <30535579+viktaur@users.noreply.github.com>
2021-01-13 23:25:56 +0100

    hell yeah!

--- tests/test_wim.py
+++ tests/test_wim.py
@@ -1,12 +1,9 @@
 def wim(*words):
-        for word in words:
-            preout = str(word) + " in message.content.lower() or "
-            # nexed = preout.replace("-", " or ")
-            # x = "\n".join(line.strip() for line in nexed)
-            x = "\n".join(preout)
-            print(x)
+    a = []
+    for word in words:
+        preout = str(word) + " in message.content.lower()"
+        a.append(preout)
+    output = " or ".join(a)
+    print(output)
 
 wim('hello', 'how are you', 'hola')
-
-# x = "".join(wim('hello', 'hi', 'hola'))
-# print(x)
